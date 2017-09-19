d6d62d02c891e1474dc36a052a3c4fc15c799980
Brian Miller <jwino0004@mail.ct.edu>
2017-09-19 15:48:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 def fizzbuzz(int)
-  if int % 3 == 0 #if the number is divisible by 3
-    "Fizz" #Go Fizz
+  if int % 3 == 0 and int % 5 == 0 #if the number is divisible by 3
+    "FizzBuzz" #Go Fizz
   elsif int % 5 == 0
      "Buzz"
- elsif int % 3 == 0 and int % 5 == 0
-   "FizzBuzz"
+ elsif int % 3 == 0
+   "Fizz"
 end
 end
